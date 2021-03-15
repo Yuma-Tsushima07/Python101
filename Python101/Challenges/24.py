a166250a32078802033058361a1b9f34aa7e7306
Yuma-Tsushima Takahashi <63207324+Yuma-Tsushima07@users.noreply.github.com>
2021-03-15 13:21:02 +0000

    24th challenge completed

--- Python101/Challenges/24.py
+++ Python101/Challenges/24.py
@@ -1,49 +1,50 @@
-print("choose which side you want to calculate")
-print("      /|")
-print("     / |")
-print("    /  |")
-print(" C /   |  A")
-print("  /    |")
-print(" /     |")
-print("/______|")
-print("    B")
+print("Choose the side you want the calculate!")
+print("""            |\\
+            | \\
+            |  \\
+         A  |   \\ C
+            |    \\
+            |_____\\ 
+              B   """)
+
+
 import math
-side = input("input be like : A or B or C : ")
+side = input("The inputs are : A or B or C : ")
 side = side.upper()
 if side == "A":
     while 1:
-        B = input("enter B lengh : ")
+        B = input("Please provide the length of value B : ")
         try:
             B = float(B)
             break
-        except:
-            print("please enter an integer ....")
+        except ValueError:
+            print("Enter an integer please!")
     while 1:
-        C = input("now enter C lengh : ")
+        C = input("Please provide the length of value C : ")
         try:
             C = float(C)
             break
-        except:
-            print("please enter an integer ....")
+        except ValueError:
+           print("Enter an integer please!")
 
     A = math.sqrt(C**2 - B**2)
     print("A is {}".format(A))
 elif side == "B":
     while 1:
-        A = input("enter A lengh : ")
+        A = input("Please provide the length of value A : ")
         try:
             A = float(A)
             break
-        except:
-            print("please enter an integer ....")
+        except ValueError:
+            print("Enter an integer please!")
 
     while 1:
-        C = input("now enter C lengh : ")
+        C = input("Now enter the length of value C : ")
         try:
             C = float(C)
             break
-        except:
-            print("please enter an integer ....")
+        except ValueError:
+            print("Enter an integer please!")
 
     B = math.sqrt(C**2 - A**2)
     print("B lengh is {}".format(B))
@@ -51,21 +52,21 @@ elif side == "B":
     
 elif side=="C":
     while 1:
-        B = input("enter B lengh : ")
+        B = input("Now enter the length of value B : ")
         try:
             B = float(B)
             break
-        except:
-            print("please enter an integer ....")
+        except ValueError:
+            print("Enter an integer please!")
 
     while 1:
-        A = input("now enter A lengh : ")
+        A = input("Now enter the length of value A: ")
         try:
             A = float(A)
             break
-        except:
-            print("please enter an integer ....")
+        except ValueError:
+            print("Enter an integer please!")
     C = math.sqrt(A**2 + B**2)
     print("C lengh is {}".format(C))
 else:
-    print("wrong input buddy , i said : A , B or C ! :(")
+    print("You have choose a wrong input. Options: A, B or C")
